139c7051a76a73eafeda1ac6ffe65797d7e7738a
Benjamin <benjidfer@gmail.com>
2023-08-09 21:30:33 -0300

    primer commit

---
+++ src/main/java/com/servidor/gestiondeventas/GestionDeVentasApplication.java
@@ -0,0 +1,13 @@
+package com.servidor.gestiondeventas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDeVentasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionDeVentasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/servidor/gestiondeventas/GestionDeVentasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.servidor.gestiondeventas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDeVentasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
