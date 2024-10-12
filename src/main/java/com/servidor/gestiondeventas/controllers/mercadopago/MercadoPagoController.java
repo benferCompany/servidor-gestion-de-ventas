d308c821df5107a92b90bc1bd06befb8a5fd3b7c
=Benjamin <bejidfer@gmail.com>
2024-10-12 01:45:42 -0300

    correciones

--- src/main/java/com/servidor/gestiondeventas/controllers/mercadopago/MercadoPagoController.java
+++ src/main/java/com/servidor/gestiondeventas/controllers/mercadopago/MercadoPagoController.java
@@ -5,12 +5,8 @@ import com.mercadopago.exceptions.MPApiException;
 import com.mercadopago.exceptions.MPException;
 
 import com.mercadopago.resources.payment.Payment;
-import com.mercadopago.resources.payment.PaymentItem;
 import com.mercadopago.resources.preference.Preference;
 import com.servidor.gestiondeventas.entities.mercadopago.WebhookEvent;
-import com.servidor.gestiondeventas.entities.mercadopago.payments.Items;
-import com.servidor.gestiondeventas.entities.mercadopago.payments.PaymentOrder;
-import com.servidor.gestiondeventas.entities.mercadopago.payments.dto.PaymentOrderDTO;
 import com.servidor.gestiondeventas.entities.receipts.Details;
 import com.servidor.gestiondeventas.services.mercadopago.MercadoPagoService;
 import com.servidor.gestiondeventas.services.mercadopago.WebhookEventService;
@@ -24,7 +20,6 @@ import org.springframework.web.bind.annotation.*;
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.*;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/mercadoPago")
@@ -35,33 +30,10 @@ public class MercadoPagoController {
     private final WebhookEventService webhookEventService;
 
     @GetMapping("getPayments/{email}")
-    public ResponseEntity<List<PaymentOrderDTO>> getPaymentsByEmail(@PathVariable String email)
+    public ResponseEntity<List<Payment>> getPaymentsByEmail(@PathVariable String email)
             throws MPException, MPApiException {
         List<Payment> response = mercadoPagoService.getPayment(webhookEventService.getWebhooksByEmail(email));
-        List<PaymentOrder> paymentOrders = new ArrayList<>();
-        paymentOrders = response.stream().map(rs->{
-            PaymentOrder paymentOrder = new PaymentOrder();
-            List<PaymentItem> paymentItems = rs.getAdditionalInfo().getItems();
-            List<Items> items = new ArrayList<>();
-
-            paymentOrder.setId(rs.getId().toString());
-            paymentOrder.setStatus(rs.getStatus());
-            paymentOrder.setStatusDetail(rs.getStatusDetail());
-            items =paymentItems.stream().map(pmitm ->{
-                Items item = new Items();
-                item.setCategoryId(pmitm.getCategoryId());
-                item.setDescription(pmitm.getDescription());
-                item.setId(Long.parseLong(pmitm.getId()));
-                item.setPictureUrl(pmitm.getPictureUrl());
-                item.setQuantity(pmitm.getQuantity());
-                item.setUnitPrice(Long.parseLong(pmitm.getUnitPrice().toString()));
-                return item;
-            }).collect(Collectors.toList());
-            paymentOrder.setItems(items);
-           return paymentOrder;
-        }).collect(Collectors.toList());
-
-        return new ResponseEntity<>(paymentOrders.stream().map(PaymentOrderDTO::fromEntity).collect(Collectors.toList()), HttpStatus.OK);
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     @PostMapping
